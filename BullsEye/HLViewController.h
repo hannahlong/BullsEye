d863d4e1e2439c6b3d90cea436dd4cf438583237
Hannah Long <geraldlong@wl-221-215.wpa.claremont.edu>
2014-09-03 15:58:09 -0700

    Initial Commit

---
+++ BullsEye/HLViewController.h
@@ -0,0 +1,13 @@
+//
+//  HLViewController.h
+//  BullsEye
+//
+//  Created by Hannah Long on 9/3/14.
+//  Copyright (c) 2014 Hannah Long. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HLViewController : UIViewController
+
+@end
---
+++ BullsEye/HLViewController.m
@@ -0,0 +1,29 @@
+//
+//  HLViewController.m
+//  BullsEye
+//
+//  Created by Hannah Long on 9/3/14.
+//  Copyright (c) 2014 Hannah Long. All rights reserved.
+//
+
+#import "HLViewController.h"
+
+@interface HLViewController ()
+
+@end
+
+@implementation HLViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
