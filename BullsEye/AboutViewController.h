27445b7ac88ff719f0cc73b1019d499f179cbf2e
Hannah Long <geraldlong@wl-221-215.wpa.claremont.edu>
2014-09-07 19:10:35 -0700

    Added about page

---
+++ BullsEye/AboutViewController.h
@@ -0,0 +1,15 @@
+//
+//  AboutViewController.h
+//  BullsEye
+//
+//  Created by Hannah Long on 9/7/14.
+//  Copyright (c) 2014 Hannah Long. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+- (IBAction)close;
+
+@end
